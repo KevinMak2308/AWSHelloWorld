e7aa72ab52660a850dc8cc4c789610c5d44a1086
Kevin <72380720+KevinMak2308@users.noreply.github.com>
2021-03-24 14:55:07 +0100

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+	public static void main(String[] args) {
+	String s = "This works, and it is running through your AWS";
+	String d = "Welcome Mak";
+	System.out.println(d + " " + s);
+}
+}
